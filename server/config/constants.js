29b8ecc4ef76ad9c82342051a3d3bd6feb0a8a47
Durgesh <48durgesh.kumar@gmail.com>
2023-12-10 06:32:00 +0530

    Update constants.js

--- server/config/constants.js
+++ server/config/constants.js
@@ -1,5 +1,5 @@
 // Get the app url based on the environment
 export const APP_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://cinematica-mern-api.vercel.app/'
+    ? 'https://cinematica-mern-api.vercel.app'
     : 'http://localhost:5000';
