237465f3f7702e88176aa95fb1446a73f29fe1b6
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2023-12-17 01:37:24 +0530

    swagger UI fix

--- server/config/constants.js
+++ server/config/constants.js
@@ -4,4 +4,4 @@ export const PROD_URL = 'https://cinematica-mern-api.vercel.app';
 export const API_URL =
   process.env.NODE_ENV === 'production' ? PROD_URL : DEV_URL;
 export const SWAGGER_CSS_URL =
-  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css';
+  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css';
