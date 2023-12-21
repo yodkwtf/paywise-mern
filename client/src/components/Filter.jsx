3f2c09d3dfda7d9b9aebcb0e318c826025f73f89
Irfan <irfansalim77@gmail.com>
2023-12-22 00:24:59 +0530

    Updated the filter component
    Signed-off-by: Irfan <irfansalim77@gmail.com>

--- client/src/components/Filter.jsx
+++ client/src/components/Filter.jsx
@@ -1,9 +1,15 @@
 // Filter.jsx
-import React from "react";
+import React, { useState } from "react";
 
-const Filter = ({ genres, onFilterChange, onClose }) => {
+const Filter = ({ onClose, onApplyFilter }) => {
+    const [filterValues, setFilterValues] = useState({ genre: "", releaseYear: "", rating: "" });
+
     const handleFilterChange = (field, value) => {
-        onFilterChange(field, value);
+        setFilterValues((prevValues) => ({ ...prevValues, [field]: value }));
+    };
+
+    const handleApplyFilter = () => {
+        onApplyFilter(filterValues);
         onClose();
     };
 
@@ -11,25 +17,31 @@ const Filter = ({ genres, onFilterChange, onClose }) => {
         <div className="filter">
             <label>
                 Filter By Genre:
-                <select onChange={(e) => handleFilterChange("genre", e.target.value)}>
-                    <option value="">All Genres</option>
-                    {genres.map((genre) => (
-                        <option key={genre} value={genre}>
-                            {genre}
-                        </option>
-                    ))}
-                </select>
+                <input type="text" placeholder="Genre" onChange={(e) => handleFilterChange("genre", e.target.value)} value={filterValues.genre} />
             </label>
 
             <label>
                 Filter By Release Year:
-                <input type="number" placeholder="Year" onChange={(e) => handleFilterChange("releaseYear", e.target.value)} />
+                <input
+                    type="number"
+                    placeholder="Year"
+                    onChange={(e) => handleFilterChange("releaseYear", e.target.value)}
+                    value={filterValues.releaseYear}
+                />
             </label>
 
             <label>
                 Filter By Rating:
-                <input type="number" placeholder="Rating" onChange={(e) => handleFilterChange("rating", e.target.value)} />
+                <input
+                    type="number"
+                    placeholder="Rating"
+                    onChange={(e) => handleFilterChange("rating", e.target.value)}
+                    value={filterValues.rating}
+                />
             </label>
+
+            <button onClick={handleApplyFilter}>Apply Filter</button>
+            <button onClick={handleApplyFilter}>Clear Filter</button>
         </div>
     );
 };
