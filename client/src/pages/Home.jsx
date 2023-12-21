6a7f1eb2f8315951fad61cc438588dc85ac2fa43
Irfan <irfansalim77@gmail.com>
2023-12-21 22:23:48 +0530

    Added controls with sorting and search functionality
    Signed-off-by: Irfan <irfansalim77@gmail.com>

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,42 +1,108 @@
-import { useEffect } from 'react';
-import MovieDetails from '../components/MovieDetails';
-import MovieForm from '../components/MovieForm';
-import useMoviesContext from '../hooks/useMoviesContext';
-import useAuthContext from '../hooks/useAuthContext';
-import Loader from '../components/Loader';
+import React, { useEffect, useState } from "react";
+import MovieDetails from "../components/MovieDetails";
+import MovieForm from "../components/MovieForm";
+import useMoviesContext from "../hooks/useMoviesContext";
+import useAuthContext from "../hooks/useAuthContext";
+import Loader from "../components/Loader";
 
 const Home = () => {
-  const { movies, fetchMovies, isLoading } = useMoviesContext();
-  const { user } = useAuthContext();
+    const { movies, fetchMovies, isLoading } = useMoviesContext();
+    const { user } = useAuthContext();
 
-  useEffect(() => {
-    if (user) {
-      fetchMovies();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [user]);
+    const [sortedBy, setSortedBy] = useState(null);
+    const [sortOrder, setSortOrder] = useState("asc");
+    const [filterBy, setFilterBy] = useState({ genre: "", releaseYear: "", rating: "" });
+    const [searchTerm, setSearchTerm] = useState("");
 
-  return (
-    <main className="home">
-      <div className="content-block">
-        {movies && movies.length > 0 ? (
-          <div className="movies">
-            {movies?.map((movie) => (
-              <MovieDetails key={movie._id} movie={movie} />
-            ))}
-          </div>
-        ) : isLoading ? (
-          <Loader />
-        ) : (
-          <p>No movies added...</p>
-        )}
-      </div>
+    useEffect(() => {
+        if (user) {
+            fetchMovies();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [user]);
 
-      <div className="form-block">
-        <MovieForm />
-      </div>
-    </main>
-  );
+    const handleSortChange = (e) => {
+        const selectedSort = e.target.value;
+        setSortOrder((prevOrder) => (selectedSort === sortedBy ? (prevOrder === "asc" ? "desc" : "asc") : "asc"));
+        setSortedBy(selectedSort);
+    };
+
+    const toggleSortOrder = () => {
+        setSortOrder((prevOrder) => (prevOrder === "asc" ? "desc" : "asc"));
+    };
+
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    const filteredMovies =
+        movies &&
+        movies.filter((movie) => {
+            return (
+                (!filterBy.genre || movie.genre === filterBy.genre) &&
+                (!filterBy.releaseYear || movie.releaseYear === parseInt(filterBy.releaseYear, 10)) &&
+                (!filterBy.rating || movie.rating === parseInt(filterBy.rating, 10)) &&
+                movie.name.toLowerCase().includes(searchTerm.toLowerCase())
+            );
+        });
+
+    const sortedMovies = (filteredMovies || []).sort((a, b) => {
+        if (sortedBy === "name") {
+            return sortOrder === "asc" ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name);
+        } else if (sortedBy === "releaseYear") {
+            return sortOrder === "asc" ? a.releaseYear - b.releaseYear : b.releaseYear - a.releaseYear;
+        } else if (sortedBy === "rating") {
+            return sortOrder === "asc" ? a.rating - b.rating : b.rating - a.rating;
+        } else if (sortedBy === "runtime") {
+            return sortOrder === "asc" ? a.runtime - b.runtime : b.runtime - a.runtime;
+        }
+        // Add more cases if needed for other sorting options
+        return 0;
+    });
+
+    return (
+        <main className="home">
+            <div className="content-block">
+                <div className="controls">
+                    <label>
+                        Sort By:
+                        <select onChange={handleSortChange} value={sortedBy}>
+                            <option value="">-- Select --</option>
+                            <option value="name">Name</option>
+                            <option value="releaseYear">Release Year</option>
+                            <option value="runtime">Runtime</option>
+                            <option value="rating">Rating</option>
+                        </select>
+                        {sortedBy && (
+                            <button className="sort-order" onClick={toggleSortOrder}>
+                                {sortOrder === "asc" ? "🔼" : "🔽"}
+                            </button>
+                        )}{" "}
+                    </label>
+
+                    <label>
+                        <input type="text" placeholder="Search..." onChange={handleSearchChange} value={searchTerm} />
+                    </label>
+                </div>
+
+                {sortedMovies.length > 0 ? (
+                    <div className="movies">
+                        {sortedMovies.map((movie) => (
+                            <MovieDetails key={movie._id} movie={movie} />
+                        ))}
+                    </div>
+                ) : isLoading ? (
+                    <Loader />
+                ) : (
+                    <p>No movies found...</p>
+                )}
+            </div>
+
+            <div className="form-block">
+                <MovieForm />
+            </div>
+        </main>
+    );
 };
 
 export default Home;
